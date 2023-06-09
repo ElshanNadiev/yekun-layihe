09ba98585a5dbb814cd34a1be4bf367f455fbcd5
ElshanNadiev13 <platin618@gmail.com>
2023-06-09 11:45:27 +0400

    finished

--- abituriyent.js
+++ abituriyent.js
@@ -7,9 +7,29 @@ added.forEach(item => {
         <p>${item.kitab}<br>${item.elaqe}</p>
         <br>
         <h4>${item.qiymet} azn</h4>
-        <p class="al1"><a class="al" href="#">Sifariş et</a></p>
+        <p class="al1">
+            <button class="delete">Sil</button>
+            <a class="al" href="#">Sifariş et</a>
+        </p>
     </div>`;
     div.innerHTML += temp    
-    
+    const btns = document.querySelectorAll('.delete');
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const book = e.target.parentElement.parentElement.children[0].src;
+            let id = -1;
+            for(let i=0; i<added.length; i++) {
+                if(added[i].sekil === book) {
+                    id = i;
+                    break;
+                }
+            }
+            if(id >= 0) {
+                added.splice(id, 1);
+                localStorage.setItem('added', JSON.stringify(added));
+                location.reload();
+            }
+        })
+    })
 })
 
